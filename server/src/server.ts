798ce7618bacbebc3ac64de03731d7dea8d3995a
dcmunhoz <dc.munhoz@hotmail.com>
2020-06-02 13:48:44 -0300

    Day 2: Back-end build
     builded backend project with node.js. Using sqlite as database and knex
     to manage them

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,29 @@
 import express from 'express';
+import path from 'path';
+import routes from './routes';
+import cors from 'cors';
+
+//Rota: Endereço completo da nossa requisição
+//Recurso: Qual entidade estamos acessando do sistema
+
+//GET: Buscar uma ou mais informações do back-end
+//POST: Criar uma nova informação no back-end
+//PUT: Atualizar uma informação existente no back-end
+//DELETE: Remover uma informação do back-end
+
+// POST http://localhost:3333/users = Criar um usuário
+// GET http://localhost:3333/users = Listar um usuário
+// GET http://localhost:3333/users/5 = Buscar dados do usuário com id 5
+
+// Request Param: Parâmetros que vem na própria rota que indetificam um recurso
+// Query Param: Parâmetros que vem na própria rota geralmente opcionais 
+// Request Body: Parâmetros para criação/atualização de dados
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get('/users', (request, respose) => {
-    console.log("listagem de usuário");
-
-    respose.json([
-        'Daniel',
-        'Diego',
-        'Filipe',
-        'Claiton'
-    ]);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes); 
 
 app.listen(3333);
